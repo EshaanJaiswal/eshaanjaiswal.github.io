54073ecdd6dba2cdbd46d7fb6925fb3c253de26c
EshaanJaiswal <eshaanjaiswal06@gmail.com>
2026-01-30 22:58:45 +0000

    Deploying to gh-pages from @ EshaanJaiswal/eshaanjaiswal.github.io@b33ae4ec22cb484779a11d3ac9cbd023f2d6cf2c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,7 +511,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets%5Cpdf%5CEshaan_Jaiswal_Resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
